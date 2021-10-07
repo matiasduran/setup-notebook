b47f4eed12321ea02f01f70160c1c39ff5cc54e2
Matías Durán <matias.duran@usach.cl>
2021-10-07 20:41:21 -0300

    install dev utils

---
+++ setup_dev_apps.sh
@@ -0,0 +1,51 @@
+#### Dev and DataScience tools ####
+
+# Install VSCode
+firefox https://code.visualstudio.com/
+
+# Install venv
+python3 -m pip install --upgrade pip
+pip3 install virtualenv
+
+# Install Docker and docker-compose
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker run hello-world
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
+
+# Install Dbeaver
+wget https://dbeaver.io/files/dbeaver-ce_latest_amd64.deb
+sudo dpkg -i dbeaver-ce_latest_amd64.deb
+
+# Install Postgres + PostGIS
+# installed with docker
+
+# RStudio
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2021.09.0-351-amd64.deb
+sudo dpkg -i rstudio-2021.09.0-351-amd64.deb
+
+#### GIS related tools ####
+
+# Install QGIS
+sudo apt install gnupg software-properties-common
+wget -qO - https://qgis.org/downloads/qgis-2021.gpg.key | sudo gpg --no-default-keyring --keyring gnupg-ring:/etc/apt/trusted.gpg.d/qgis-archive.gpg --import
+sudo chmod a+r /etc/apt/trusted.gpg.d/qgis-archive.gpg
+sudo add-apt-repository "deb https://qgis.org/ubuntu $(lsb_release -c -s) main"
+sudo apt update
+sudo apt install qgis qgis-plugin-grass
