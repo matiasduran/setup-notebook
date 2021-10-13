4741fc863499ff53e70e5c125ee53a3ae606224a
Matias <matias.duran@usach.cl>
2021-10-13 11:36:17 -0300

    add libreoffice suite

--- setup_basic_apps.sh
+++ setup_basic_apps.sh
@@ -32,4 +32,7 @@ sudo apt install ffmpeg
 sudo apt install v4l2loopback-dkms
 sudo add-apt-repository ppa:obsproject/obs-studio
 sudo apt update
-sudo apt install obs-studio
+sudo apt install obs-studiox
+
+# Install LibreOffice
+sudo apt install libreoffice-gnome libreoffice
